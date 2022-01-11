62a9167cb2dd4c135f051f046d609954be80f3c1
mohamadnour badr <mohamadbader20@yahoo.com>
2022-01-11 04:13:21 +0100

    create class library and start with forms

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        /// <summary>
+        /// Repesent one team in the match
+        ///
+        /// </summary>
+        public TeamModel Teamcompeting { get; set; }
+        public double Score { get; set; }
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
